482583bfa7ced13f315add10a8ca5ab32f1100ec
simonstuden <sstuden@cs.washington.edu>
2022-05-05 13:43:07 -0700

    Fixed localStorage bug

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 let count = 0
-const countFromLocalStorage = localStorage.getItem("count")
+const countFromLocalStorage = parseInt(localStorage.getItem("count"))
 const counter = document.getElementById("count")
 const incrementBtn = document.getElementById("increment-btn")
 const resetBtn = document.getElementById("reset-btn")
 
 if (countFromLocalStorage > 0) {
-    count = countFromLocalStorage
+    count =countFromLocalStorage
     render(count)
 }
 
 function render(count) {
-    counter.innerHTML = `<p> Count: ${count} </p>`
+    counter.innerHTML = `Count: ${count}`
 }
 
 incrementBtn.addEventListener("click", function(){
