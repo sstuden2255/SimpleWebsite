f6ef1769966bc4a030208f372532ad68178f144a
simonstuden <sstuden@cs.washington.edu>
2022-05-05 11:10:00 -0700

    Basic counter working

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+let count = 0
+const countFromLocalStorage = localStorage.getItem("count")
+const counter = document.getElementById("count")
 const incrementBtn = document.getElementById("increment-btn")
+const resetBtn = document.getElementById("reset-btn")
+
+if (countFromLocalStorage > 0) {
+    count = countFromLocalStorage
+    render(count)
+}
+
+function render(count) {
+    counter.innerHTML = `<p> Count: ${count} </p>`
+}
 
 incrementBtn.addEventListener("click", function(){
-    /* increment counter 
-       render count
-    */
+    count += 1
+    localStorage.setItem("count", count.toString())
+    render(count)
+})
+
+resetBtn.addEventListener("click", function(){
+    localStorage.clear()
+    count = 0
+    render(count)
 })
